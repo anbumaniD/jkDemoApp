54d4c6fefd0830c06e40ac1a1f14a933401f352a
anbumanid <newanbu22@yahoo.com>
2016-08-17 13:15:53 +0530

    Initial Commit

---
+++ JKDemoApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JKDemoApp
+//
+//  Created by Anbumani on 17/08/16.
+//  Copyright © 2016 HCL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
